bf975831207ab3fd7f78e3dd215c4bc5223f08da
Void-development <void@memeware>
2020-03-21 22:10:13 +0100

    cleanup code

--- voidframework_compact/initialize.cpp
+++ voidframework_compact/initialize.cpp
@@ -8,15 +8,14 @@
  */
 #include "stdafx.h"
 
-
 //Using hazedumper, thanks alot to frk1
 //https://github.com/frk1/hazedumper
 #include "hazedumper.hpp"
 using namespace hazedumper::netvars;
 using namespace hazedumper::signatures;
 
-HANDLE procHandle;
-DWORD procId;
+HANDLE processHandle;
+DWORD processIdentifier;
 DWORD dwPanoramaClient;
 DWORD dwEngine;
 int iClientState;
@@ -40,40 +39,18 @@ DWORD GetModuleBaseAdress(LPSTR lpModuleName, DWORD dwProcessId)
 	CloseHandle(handleSnap);
 	return NULL;
 }
-DWORD playerBase;
-int hp;
-int m_flags;
-int teamnum;
-int gEntTeam;
-bool gEntDormant;
 
+DWORD playerBase;
+//->
+int localPlayerTeam;
 int xhairEntity;
+
+
+
 DWORD EntityBase;
+//->
 int EntityTeam;
 int EntityHealth;
-
-DWORD WINAPI Triggerbot(LPVOID PARAMS) {
-	while (true) {
-		
-			while (GetAsyncKeyState(0x5)) {
-				ReadProcessMemory(procHandle, (LPVOID)(dwPanoramaClient + dwLocalPlayer), &playerBase, sizeof(playerBase), NULL);
-				ReadProcessMemory(procHandle, (LPVOID)(playerBase + 0xF4), &teamnum, sizeof(teamnum), NULL);
-				ReadProcessMemory(procHandle, (LPVOID)(playerBase + m_iCrosshairId), &xhairEntity, sizeof(xhairEntity), NULL);
-				ReadProcessMemory(procHandle, (LPVOID)(dwPanoramaClient + dwEntityList + (xhairEntity - 1) * 0x10), &EntityBase, sizeof(EntityBase), NULL);
-				ReadProcessMemory(procHandle, (LPVOID)(EntityBase + 0xF4), &EntityTeam, sizeof(EntityTeam), NULL);
-				ReadProcessMemory(procHandle, (LPVOID)(EntityBase + m_iHealth), &EntityHealth, sizeof(EntityHealth), NULL);
-				if (xhairEntity > 0) {
-					if (EntityTeam != teamnum && EntityHealth > 0) {
-
-						mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, NULL, NULL, NULL, NULL);
-					}
-				}
-				Sleep(1);
-			}		
-	}
-	return 0;
-}
-
 
 //To write color struct in 1 operation.
 struct colors
@@ -84,27 +61,47 @@ struct colors
 	BYTE a;
 };
 
+//hwnd
+HWND hwndCstrikeWindow = NULL;
+
+DWORD WINAPI Triggerbot(LPVOID PARAMS) {
+	while (true) {
+			while (GetAsyncKeyState(0x5)) {
+				ReadProcessMemory(processHandle, (LPVOID)(dwPanoramaClient + dwLocalPlayer), &playerBase, sizeof(playerBase), NULL);
+				ReadProcessMemory(processHandle, (LPVOID)(playerBase + 0xF4), &localPlayerTeam, sizeof(localPlayerTeam), NULL);
+				ReadProcessMemory(processHandle, (LPVOID)(playerBase + m_iCrosshairId), &xhairEntity, sizeof(xhairEntity), NULL);
+				ReadProcessMemory(processHandle, (LPVOID)(dwPanoramaClient + dwEntityList + (xhairEntity - 1) * 0x10), &EntityBase, sizeof(EntityBase), NULL);
+				ReadProcessMemory(processHandle, (LPVOID)(EntityBase + 0xF4), &EntityTeam, sizeof(EntityTeam), NULL);
+				ReadProcessMemory(processHandle, (LPVOID)(EntityBase + m_iHealth), &EntityHealth, sizeof(EntityHealth), NULL);
+				if (xhairEntity > 0) {
+					if (EntityTeam != localPlayerTeam && EntityHealth > 0) {
+
+						mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, NULL, NULL, NULL, NULL);
+					}
+				}
+				Sleep(1);
+			}		
+	}
+	return 0;
+}
 
 DWORD WINAPI CheatEntryPoint(LPVOID PARAMS) {
 
 		std::cout << "VoidFramework by Void-Development\n";
 		std::cout << "www.github.com/voidzero-development\n";
-
 		std::cout << "Waiting for CS:GO!\n";
 
-		HWND windowHandle = NULL;
-
 		do {
-			windowHandle = FindWindowA(0, "Counter-Strike: Global Offensive");
-		} while (windowHandle == NULL);
+			hwndCstrikeWindow = FindWindowA(0, "Counter-Strike: Global Offensive");
+		} while (hwndCstrikeWindow == NULL);
 
-		GetWindowThreadProcessId(windowHandle, &procId);
-		procHandle = OpenProcess(PROCESS_ALL_ACCESS, 0, procId);
+		GetWindowThreadProcessId(hwndCstrikeWindow, &processIdentifier);
+		processHandle = OpenProcess(PROCESS_ALL_ACCESS, 0, processIdentifier);
 
 		Sleep(10000);
 
-		dwPanoramaClient = GetModuleBaseAdress((LPSTR)"client_panorama.dll", procId);
-		dwEngine = GetModuleBaseAdress((LPSTR)"engine.dll", procId);
+		dwPanoramaClient = GetModuleBaseAdress((LPSTR)"client_panorama.dll", processIdentifier);
+		dwEngine = GetModuleBaseAdress((LPSTR)"engine.dll", processIdentifier);
 
 		std::cout << "Panorama Client: " << dwPanoramaClient << std::endl;
 		std::cout << "Engine: " << dwEngine << std::endl;
